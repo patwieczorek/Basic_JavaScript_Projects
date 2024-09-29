99013e0191c707588bb577b3bdf0094eab33c3b1
patwieczorek <pat.wieczorek02@gmail.com>
2024-09-29 20:28:38 +0100

    Initial commit

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+window.alert("Hello, World!");
+var Sent1 ="This is the beginning of a string";//declaring first variable
+document.write(Sent1);
+var Sent2= "and this is the end of string";//declaring second variable
+window.alert(Sent2);
+let text= "We are the so-called \"Car Specialists\" group.";
+document.write(text);
+document.write("\"We have a range of beautiful cars,"
+    +"anything from 10 yers old to brand new.\""
++"You\'ll definitelly find something right for your budget."); //combing lines of text that will print together 
+var All= Sent1 + Sent2; //concatenating the Sent1 and Sent2 variables together
+document.write(All);
+var newCar = "Audi", usedCar= "Ford", electricCar= "Tesla";
+document.write(electricCar); //print one of the variables form the list 
+document.write(3+2); //creating an expression
