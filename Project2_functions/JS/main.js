e79e1db7c01cc093bac3c206c25f29a760b986ee
patwieczorek <pat.wieczorek02@gmail.com>
2024-09-29 21:22:56 +0100

    updated

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -2,13 +2,13 @@ var daysInAWeeek=7;
 var weeksInAYear=52;
 function daysInAYear(daysInAWeek, weeksInAYear) {
     return daysInAWeeek * weeksInAYear;
-}
+} //creating a function to calculate days in a year
 let result = daysInAYear(7, 52);
 document.getElementById("demo").innerHTML = result;
 document.write("We have this many days in a year.");
 
 function myFunction() {
     var sentence= "I am learning";
-    sentence += "a lot from this book!";
-    document.getElementById("Concatenate") .innerHTML=sentence;
+    sentence += "a lot from this book!"; //writing function with += operator combining two lines of text
+    document.getElementById("Concatenate") .innerHTML=sentence; //linking it to the correct Id
 }
